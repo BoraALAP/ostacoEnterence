f34d8132bfe6ff4fb1384615a313bc2694decc94
BoraALAP <alapbora@gmail.com>
2020-06-24 23:00:43 -0400

    build

---
+++ build/precache-manifest.a8a7998a3c34b880c4a6223ca6bca271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c692537e27554b8382fbb8826c0b3a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e97591e6c4900b53229",
+    "url": "/static/js/2.2bdd66d3.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.2bdd66d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b849853f0496938b5a97",
+    "url": "/static/js/main.7e2fabaf.chunk.js"
+  },
+  {
+    "revision": "e0a680eb0ceaa3f7d6e3",
+    "url": "/static/js/runtime-main.498e6f6b.js"
+  }
+]);
