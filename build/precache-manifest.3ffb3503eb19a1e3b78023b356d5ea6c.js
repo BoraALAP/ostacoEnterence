de4a6c89384c4e9331a58c8182811091f40c3bfb
BoraALAP <alapbora@gmail.com>
2020-06-28 14:50:31 -0400

    testing homepage

---
+++ build/precache-manifest.3ffb3503eb19a1e3b78023b356d5ea6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee279bb31c509bf3ffe90c7ab8ba18f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a3bd53233a74ab43f48",
+    "url": "./static/js/2.5aacc669.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "./static/js/2.5aacc669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e03b38d6b21def6a63",
+    "url": "./static/js/main.1d03db2e.chunk.js"
+  },
+  {
+    "revision": "ee24d771f79dedf6fa4f",
+    "url": "./static/js/runtime-main.961994d8.js"
+  },
+  {
+    "revision": "74f780379b6182b5ddefabad9e70b73a",
+    "url": "./static/media/favicon.74f78037.ico"
+  },
+  {
+    "revision": "e38462f919a16b06e289130774eb6798",
+    "url": "./static/media/safari-pinned-tab.e38462f9.svg"
+  },
+  {
+    "revision": "55eafa23e4638f0683d6997d04170ade",
+    "url": "./static/media/site.55eafa23.webmanifest"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1351b8724b2a2296d6aef240992fbb57.js"
+  "./precache-manifest.3ffb3503eb19a1e3b78023b356d5ea6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
