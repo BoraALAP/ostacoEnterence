ef511e02d8041240879acb711fe5a89235b0de0b
BoraALAP <alapbora@gmail.com>
2020-06-28 14:47:07 -0400

    Updates

---
+++ precache-manifest.dce1c0e04e8fa55f80f3d8d37124757b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c297b79e68314c5b777e1d7159e1ff",
+    "url": "/ostacoEnterence/index.html"
+  },
+  {
+    "revision": "0b71f420de04cb233edd",
+    "url": "/ostacoEnterence/static/js/2.7c781b1e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/ostacoEnterence/static/js/2.7c781b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1285d3df8402900702",
+    "url": "/ostacoEnterence/static/js/main.cf128734.chunk.js"
+  },
+  {
+    "revision": "68ce86a9ab7c43e52df2",
+    "url": "/ostacoEnterence/static/js/runtime-main.ecd0fb95.js"
+  },
+  {
+    "revision": "74f780379b6182b5ddefabad9e70b73a",
+    "url": "/ostacoEnterence/static/media/favicon.74f78037.ico"
+  },
+  {
+    "revision": "e38462f919a16b06e289130774eb6798",
+    "url": "/ostacoEnterence/static/media/safari-pinned-tab.e38462f9.svg"
+  },
+  {
+    "revision": "55eafa23e4638f0683d6997d04170ade",
+    "url": "/ostacoEnterence/static/media/site.55eafa23.webmanifest"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1351b8724b2a2296d6aef240992fbb57.js"
+  "/ostacoEnterence/precache-manifest.dce1c0e04e8fa55f80f3d8d37124757b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ostacoEnterence/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
